87783e6d7317dd01ee94762e6de35eae78854a1a
domhnall-091labs <domhnall@091labs.com>
2013-01-10 02:57:18 +0000

    Update Wiegand Reader Test (Arduino)/wiegand_test.ino
    Changed 'buffer' to 'keyBuffer' and confirmed that the code indeed works. Next stop, port it to a Raspberry Pi.

--- Wiegand Reader Test (Arduino)/wiegand_test.ino
+++ Wiegand Reader Test (Arduino)/wiegand_test.ino
@@ -6,20 +6,21 @@
 // Count the number of bits received.
 volatile int bitCount = 0;
 
-// A 32-bit-long buffer into which the number is stored.
-unsigned long keyNumber = 0;
+// A 32-bit-long keyBuffer into which the number is stored.
+unsigned long keyBuffer = 0;
 
 // Data in pins for the Weigand protocol
-int data0Pin = 2;     // INT0
-int data1Pin = 3;     // INT1
-int ledPin = 13;      // Light an LED to signify that a code has been outputted
-int codeSize = 26;    // Assuming it's the Wiegand 26 protocol, there are 26 bits per number.
+int data0Pin = 2; // INT0
+int data1Pin = 3; // INT1
+int ledPin = 13; // Light an LED to signify that a code has been outputted
+int codeSize = 26; // Assuming it's the Wiegand 26 protocol, there are 26 bits per number.
 
 void setup()
 {
   // Set the two data pins as inputs
   pinMode(data0Pin, INPUT);
   pinMode(data1Pin, INPUT);
+  
   pinMode(ledPin, OUTPUT);
   
   // Attach interrupts to the two pins
@@ -36,51 +37,50 @@ void setup()
 void recv0()
 {
   // We've received a 0 bit.
-  receive_bit(0);
+  // Left shift the number (effectively multiplying by 2)
+  keyBuffer = keyBuffer << 1;
+  // Increment the bit count
+  bitCount++;
 }
 
 void recv1()
 {
   // We've received a 1 bit.
-  receive_bit(1); 
-}
-
-void receive_bit(byte value)
-{
-  // Left shift the key number by one bit...
-  keyNumber << 1;
-  keyNumber |= value;
+  // Left shift the number (effectively multiplying by 2)
+  keyBuffer = keyBuffer << 1;
+  // Add the 1 (not necessary for the zeroes)
+  keyBuffer += 1;
+  // Increment the bit count
   bitCount++;
 }
 
 void loop()
 {
-  unsigned long buffer;
   // If we have the required number of bits, then we _should_ have
   // a complete key. Out it goes...
   if (bitCount == codeSize)
   {
-    // Read in the current key and reset everything so that the interrupts can 
+    // Read in the current key and reset everything so that the interrupts can
     // carry on in the background.
-    buffer = keyNumber;
-    keyNumber = 0;
     bitCount = 0;
 
-    // Strip leading and trailing parity bits from the buffer.
-    buffer >> 1;
-    bitClear(buffer, 24);
+    // Strip leading and trailing parity bits from the keyBuffer. For now, we're ignoring them.
+    keyBuffer = keyBuffer >> 1;
+    keyBuffer &= 0xFFFFFF;
     
     // Light the LED
     digitalWrite(ledPin, HIGH);
-    
-    // Send the key number over the serial interface so that we can check it at 
-    // the computer.
-    Serial.println(buffer);
+        
+    Serial.print("Got key: ");
+    Serial.println(keyBuffer);
 
     // Wait 100ms so that the LED is visible as having pulsed.
     delay(100);
 
     // Turn off the LED
-    digitalWrite(ledPin, LOW);    
+    digitalWrite(ledPin, LOW);
+    
+    // Clear the buffer for the next iteration.
+    keyBuffer = 0;
   }
 }
